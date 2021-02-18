75c220c4a8c2cce9a869348ed06912fbe34fc86e
Khomsan Chuechuphaeng <soua214@gmail.com>
2021-02-18 22:44:01 +0700

    [fix&update]preview

--- INT203_62130500012_GroupWork_4/main.js
+++ INT203_62130500012_GroupWork_4/main.js
@@ -3,20 +3,24 @@ const app = {
         return {
             tasks: [
                 {
+                    no : 0 ,
                     url: 'https://images.unsplash.com/photo-1610002266468-d523466ad587?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
                     title: "Almora, Uttarakhand, India",
                     done: false,
                 },
                 {
+                    no : 1 ,
                     url: 'https://images.unsplash.com/photo-1612275176266-8166c0479604?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
                     title: "Switzerland", done: false
                 },
                 {
+                    no : 2 ,
                     url: 'https://images.unsplash.com/photo-1581665611754-08d420f9590f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
                     title: "Приморский край, Россия",
                     done: false,
                 },
                 {
+                    no : 3 ,
                     url: 'https://images.unsplash.com/photo-1587772989305-22eaf5c9f355?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
                     title: "Золотая Долина, Приморский край, Россия",
                     done: false,
@@ -31,14 +35,16 @@ const app = {
         };
     },
     methods: {
-        like(index) {
-            this.tasks[index].done = !this.tasks[index].done;
+        like(task) {
+            // this.tasks[index].done = !this.tasks[index].done;
+            this.tasks[task.no].done = !this.tasks[task.no].done;
         },
         search() {
             this.form.search = !this.form.search;
         },
-        toggleView(index) {
-            this.viewImg = this.tasks[index].url;
+        toggleView(task) {
+            // this.viewImg = this.tasks[index].url;
+            this.viewImg = this.tasks[task.no].url;
             this.hasView = true;
         },
         cancel() {
